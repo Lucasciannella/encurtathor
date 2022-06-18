e7c63cda639988e5f8bbf9f853b70e2bd01d03f0
Lucas Ciannella de Oliveira <lucasciannella@hotmail.com>
2022-06-18 12:32:41 -0300

    start project

---
+++ src/main/java/br/com/encurtathor/encurtador/EncurtadorDeLinksApplication.java
@@ -0,0 +1,13 @@
+package br.com.encurtathor.encurtador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorDeLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorDeLinksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/encurtathor/encurtador/EncurtadorDeLinksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.encurtathor.encurtador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorDeLinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
